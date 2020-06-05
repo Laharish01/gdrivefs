142895d0f164cb13c70c5276da520bf475dbe914
Laharish01 <laharish2k17@gmail.com>
2020-06-05 19:58:18 +0530

    Edited mount.sh to check for directory presence

--- mount.sh
+++ mount.sh
@@ -1,3 +1,17 @@
 
+default_directory="/home/${USER}/gdrivefs-backup"
+
+if [ $# -eq 0 ]
+    then
+        echo "Please exit and enter a valid directory or enter c for default directory"
+        read c
+        echo "Creating directory..."
+        [ ! -d "${default_directory}" ] && mkdir -p "${default_directory}"
+fi
+
 rm -rf src/root
-python3 src/fusion.py $1
+
+#echo ${1}
+#echo ${default_directory}
+#echo ${1:-${default_directory}}
+python3 src/fusion.py ${1:-${default_directory}}
